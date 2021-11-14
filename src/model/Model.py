67324dab9b8258b8a95ae1db8830ccbfa17385fe
Lorenzo Bucci <lorenzo.bucci@stud.unifi.it>
2021-11-14 21:35:46 +0100

    Aggiunta al modello la lettura dei tag EXIF

--- src/model/Model.py
+++ src/model/Model.py
@@ -1,20 +1,30 @@
 import sys
 
 from pathlib import Path
+from PIL import Image, ExifTags
 
 
 class Model:
     def __init__(self):
         self.currentImage = Path(sys.argv[1])
+        self._updateEXIFData()
 
         self.imagesList = sorted(self.currentImage.parent.glob("*.jpg"))
         self.imagesIterator = BidirectionalIterator(self.imagesList, self.imagesList.index(self.currentImage))
 
     def getPreviousImage(self) -> Path:
-        return self.imagesIterator.prev()
+        self.currentImage = self.imagesIterator.prev()
+        self._updateEXIFData()
+        return self.currentImage
 
     def getNextImage(self) -> Path:
-        return self.imagesIterator.next()
+        self.currentImage = self.imagesIterator.next()
+        self._updateEXIFData()
+        return self.currentImage
+
+    def _updateEXIFData(self):
+        imageWithEXIF = Image.open(self.currentImage)
+        self.currentEXIFData = {ExifTags.TAGS[k]: v for k, v in imageWithEXIF._getexif().items() if k in ExifTags.TAGS}
 
 
 class BidirectionalIterator:
