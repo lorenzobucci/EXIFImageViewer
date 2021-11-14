2488ee05ea83f2f519b647d7c30eae5d23b0cf2b
Lorenzo Bucci <lorenzo.bucci@stud.unifi.it>
2021-11-14 11:45:30 +0100

    Aggiunta altezza minima finestra

--- src/view/ImageViewer.py
+++ src/view/ImageViewer.py
@@ -13,6 +13,7 @@ class ImageViewer(QtWidgets.QMainWindow):
 
         self.setObjectName("ImageViewer")
         self.resize(1022, 616)
+        self.setMinimumHeight(200)
 
         self.centralwidget = QtWidgets.QWidget(self)
         self.centralwidget.setObjectName("centralwidget")
