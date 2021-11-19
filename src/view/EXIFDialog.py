9939d4f6ead8d2844ae27ec1f54776c4bff5c0f2
Lorenzo Bucci <lorenzo.bucci@stud.unifi.it>
2021-11-19 16:02:05 +0100

    Miglioramenti visivi dialogo

--- src/view/EXIFDialog.py
+++ src/view/EXIFDialog.py
@@ -1,4 +1,5 @@
-from PyQt5 import QtCore, QtWidgets
+from PyQt5 import QtCore, QtWidgets, Qt
+from PyQt5.QtWidgets import QAbstractItemView
 
 from src.model import QEXIFModel
 
@@ -7,7 +8,7 @@ class EXIFDialog(QtWidgets.QDialog):
     def __init__(self, exifData: dict):
         super().__init__()
         self.setObjectName("Dialog")
-        self.resize(330, 390)
+        self.resize(345, 450)
         self.setWindowTitle("Dati EXIF")
 
         self.gridLayout = QtWidgets.QGridLayout(self)
@@ -16,6 +17,11 @@ class EXIFDialog(QtWidgets.QDialog):
         self.tableView = QtWidgets.QTableView(self)
         self.tableView.setObjectName("tableView")
         self.tableView.setAlternatingRowColors(True)
+        self.tableView.setSelectionMode(QAbstractItemView.SingleSelection)
+        self.tableView.horizontalHeader().setHighlightSections(False)
+        self.tableView.horizontalHeader().setDefaultSectionSize(150)
+        self.tableView.setHorizontalScrollMode(QAbstractItemView.ScrollPerPixel)
+        self.tableView.verticalHeader().setDefaultSectionSize(10)
         self.tableView.setModel(QEXIFModel.QEXIFModel(exifData))
         self.gridLayout.addWidget(self.tableView, 0, 0, 1, 1)
 
