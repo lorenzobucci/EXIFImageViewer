d5c6b8b7512c548c6d39cd0ad306bf9550aab45d
Lorenzo Bucci <lorenzo.bucci@stud.unifi.it>
2022-03-13 14:08:13 +0100

    Aggiunta documentazione main

--- src/main.py
+++ src/main.py
@@ -7,8 +7,13 @@ from model.Model import Model
 from view.ImageViewer import ImageViewer
 
 app = QApplication(sys.argv)
+
+# Inizializzazione modello, vista e relativo controller per dependency injection
 model = Model()
 mainWindow = ImageViewer()
 controller = Controller(mainWindow, model)
+
+# Lancio finestra principale PyQt
+# Vengono passati i parametri ricevuti da riga di comando all'intera applicazione
 mainWindow.show()
 sys.exit(app.exec_())
